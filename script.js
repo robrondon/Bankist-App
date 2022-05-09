22d0101898b444d45bff239080348c5d263716a2
Robert Rondon <robrondon11@gmail.com>
2022-05-09 10:02:46 -0300

    adding cookie message and implement smooth scrolling to the first section

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 'use strict';
 
-/* Modal Window */
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const allSections = document.querySelectorAll('.section');
+const allButtons = document.getElementsByTagName('button');
+const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+/***** Modal Window *****/
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -27,4 +31,34 @@ document.addEventListener('keydown', e => {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+/**** Insert & remove cookie message ****/
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies to improve functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+header.append(message);
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', () => message.remove());
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '103%';
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+/***** Implement smooth scrolling ******/
+btnScrollTo.addEventListener('click', e => {
+  // const s1coords = section1.getBoundingClientRect();
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
